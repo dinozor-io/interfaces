d3cfc2b2fa7e9793efc463937c4715b87f4a50ae
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2024-04-24 20:20:18 +0300

    Response interface updated to serve html files

--- response.go
+++ response.go
@@ -2,8 +2,10 @@ package interfaces
 
 type Response interface {
 	Init(Controller)
+	Type() int
 	Contr() Controller
 	JSON(int, map[string]any)
+	HTML(int, string)
 	StatusCode() int
 	Data() map[string]any
 }
